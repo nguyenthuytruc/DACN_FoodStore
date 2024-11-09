4392cc0a26625bfe3647cdaed5d061b0789456d3
hoai-nguyen <nguyenvanhoai.cs@gmail.com>
2024-11-09 12:13:25 +0900

    counting

--- FoodStore/Repositories/EFOrderDetailRepository.cs
+++ FoodStore/Repositories/EFOrderDetailRepository.cs
@@ -1,44 +1,54 @@
 ﻿using FoodStore.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace FoodStore.Repositories
+
+public class EFOrderDetailRepository : IOrderDetailRepository
 {
-    public class EFOrderDetailRepository : IOrderDetailRepository
+    private readonly ApplicationDbContext _context;
+
+    public EFOrderDetailRepository(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public EFOrderDetailRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public async Task<IEnumerable<OrderDetail>> GetAllAsync()
+    {
+        return await _context.OrderDetails.ToListAsync();
+    }
 
-        public async Task<IEnumerable<OrderDetail>> GetAllAsync()
-        {
-            return await _context.OrderDetails.ToListAsync();
-        }
+    public async Task<OrderDetail> GetByIdAsync(int id)
+    {
+        return await _context.OrderDetails.FindAsync(id);
+    }
 
-        public async Task<OrderDetail> GetByIdAsync(int id)
-        {
-            return await _context.OrderDetails.FindAsync(id);
-        }
+    public async Task AddAsync(OrderDetail orderDetail)
+    {
+        _context.OrderDetails.Add(orderDetail);
+        await _context.SaveChangesAsync();
+    }
 
-        public async Task AddAsync(OrderDetail orderDetail)
-        {
-            _context.OrderDetails.Add(orderDetail);
-            await _context.SaveChangesAsync();
-        }
+    public async Task UpdateAsync(OrderDetail orderDetail)
+    {
+        _context.OrderDetails.Update(orderDetail);
+        await _context.SaveChangesAsync();
+    }
 
-        public async Task UpdateAsync(OrderDetail orderDetail)
-        {
-            _context.OrderDetails.Update(orderDetail);
-            await _context.SaveChangesAsync();
-        }
+    public async Task DeleteAsync(int id)
+    {
+        var orderdetail = await _context.OrderDetails.FindAsync(id);
+        _context.OrderDetails.Remove(orderdetail);
+        await _context.SaveChangesAsync();
+    }
 
-        public async Task DeleteAsync(int id)
-        {
-            var orderdetail = await _context.OrderDetails.FindAsync(id);
-            _context.OrderDetails.Remove(orderdetail);
-            await _context.SaveChangesAsync();
-        }
+    // Đếm số món ăn chưa hoàn thành (giả sử Status = 0 là chưa hoàn thành)
+    public async Task<int> CountUnfinishedAsync()
+    {
+        return await _context.OrderDetails.CountAsync(od => od.Status == 0);
+    }
+
+    // Đếm số món ăn chờ bàn giao (giả sử Status = 1 là chờ bàn giao)
+    public async Task<int> CountWaitingForDeliveryAsync()
+    {
+        return await _context.OrderDetails.CountAsync(od => od.Status == 1);
     }
 }
--- FoodStore/Repositories/IOrderDetailRepository.cs
+++ FoodStore/Repositories/IOrderDetailRepository.cs
@@ -1,13 +1,16 @@
 ﻿using FoodStore.Models;
 
-namespace FoodStore.Repositories
+public interface IOrderDetailRepository
 {
-    public interface IOrderDetailRepository
-    {
-        Task<IEnumerable<OrderDetail>> GetAllAsync();
-        Task<OrderDetail> GetByIdAsync(int id);
-        Task AddAsync(OrderDetail order);
-        Task UpdateAsync(OrderDetail order);
-        Task DeleteAsync(int id);
-    }
+    Task<IEnumerable<OrderDetail>> GetAllAsync();
+    Task<OrderDetail> GetByIdAsync(int id);
+    Task AddAsync(OrderDetail order);
+    Task UpdateAsync(OrderDetail order);
+    Task DeleteAsync(int id);
+
+    // Phương thức đếm số món ăn chưa hoàn thành
+    Task<int> CountUnfinishedAsync();
+
+    // Phương thức đếm số món ăn chờ bàn giao
+    Task<int> CountWaitingForDeliveryAsync();
 }
